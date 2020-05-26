09bcc4f7de53a3457e4c4908d42a932ea2a1d4ce
Diogo Pinto <diogosmp@gmail.com>
2020-05-26 22:06:46 +0000

    Refactores ex 1 and 7

--- lib/store.rb
+++ lib/store.rb
@@ -3,9 +3,12 @@ class Store < ActiveRecord::Base
 
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
-  validates :must_carry_mens_womens_apparel, on: :create
+  validate :carries_mens_womens_apparel?
 
-  def must_carry_mens_womens_apparel 
-    erros.add(:name, " store doesn't carry mens and womens apparel.") unless :mens_apparel && :womens_apparel
+  def carries_mens_womens_apparel?
+    unless mens_apparel || womens_apparel
+      errors.add(:name, " store doesn't carry mens and womens apparel.")
+    end
   end
+
 end
